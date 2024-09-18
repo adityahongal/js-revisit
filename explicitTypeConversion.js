caad38cab7f6b4e7f6b842e10dd0f437c59c5050
adityahongal <adityahongalwork@gmail.com>
2024-09-18 19:29:47 +0530

    Explicit Type Conversion

---
+++ explicitTypeConversion.js
@@ -0,0 +1,13 @@
+result2 = "5";                     // string
+result2 = Number(result2);        // Explicitly declaring it as a number
+console.log(typeof result2);      // prints as number
+
+result3 = 66;                     
+result3 = String(result3);     
+console.log(result3); 
+console.log(typeof result2);
+
+result4 = -90.3;
+result4 = parseFloat(result4);
+console.log(result4);
+console.log(typeof result4);
